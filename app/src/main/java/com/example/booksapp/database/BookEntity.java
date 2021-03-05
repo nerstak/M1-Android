7f6f696441f33567816f8f4b34bf1fbcb40f3cfd
nerstak <roy.karst@gmail.com>
2021-03-06 00:02:31 +0100

    Updating database model

--- app/src/main/java/com/example/booksapp/database/BookEntity.java
+++ app/src/main/java/com/example/booksapp/database/BookEntity.java
@@ -2,13 +2,14 @@ package com.example.booksapp.database;
 
 import android.graphics.Bitmap;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class BookEntity {
-    @PrimaryKey
+    @PrimaryKey @NonNull
     public String id;
 
     @ColumnInfo(name = "title")
@@ -24,18 +25,18 @@ public class BookEntity {
     public int pageRead;
 
     @ColumnInfo(name = "status")
-    public StatusBook status;
+    public String status;
 
-    @ColumnInfo(name = "cover")
-    public Bitmap cover;
+    @ColumnInfo(name = "resume")
+    public String resume;
 
-    public BookEntity(String id, String title, String author, int pageCount, int pageRead, StatusBook status, Bitmap cover) {
+    public BookEntity(String id, String title, String author, int pageCount, int pageRead, String status, String resume) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.pageCount = pageCount;
         this.pageRead = pageRead;
         this.status = status;
-        this.cover = cover;
+        this.resume = resume;
     }
 }
