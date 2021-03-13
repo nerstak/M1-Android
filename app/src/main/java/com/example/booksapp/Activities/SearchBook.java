2a9677eab66d6fb4d7264bb12be2f83c7a465448
Unknown <theo.delettre@gmail.com>
2021-03-13 19:25:40 +0100

    UI improvements

--- app/src/main/java/com/example/booksapp/Activities/SearchBook.java
+++ app/src/main/java/com/example/booksapp/Activities/SearchBook.java
@@ -3,11 +3,14 @@ package com.example.booksapp.Activities;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.booksapp.R;
+import com.example.booksapp.database.BookDatabase;
+import com.example.booksapp.database.DatabaseUtilities;
 
 public class SearchBook extends AppCompatActivity {
 
@@ -15,6 +18,9 @@ public class SearchBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_book);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.search_header);
 
         TextView searchQueryView = (TextView) findViewById(R.id.search_query);
 
@@ -24,5 +30,18 @@ public class SearchBook extends AppCompatActivity {
             String bookQuery = searchIntent.getStringExtra(SearchManager.QUERY);
             searchQueryView.setText(bookQuery);
         }
+    }
+
+    /**
+     * Function to go back to the previous activity
+     * @param item Menu Item
+     * @return Boolean of success
+     */
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return false;
     }
 }
