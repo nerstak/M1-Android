7d0dceead3d5309ebf5a168ba22852b02c578375
Unknown <theo.delettre@gmail.com>
2021-03-19 19:51:03 +0100

    Cover Upload & :)
    (covers dont save yet)

--- app/src/main/java/com/example/booksapp/Activities/ActivityBook.java
+++ app/src/main/java/com/example/booksapp/Activities/ActivityBook.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,8 +15,10 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +40,8 @@ import java.util.stream.IntStream;
 public class ActivityBook extends AppCompatActivity {
     public BookEntity bookEntity;
     private String currentFragment = "info";
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +67,7 @@ public class ActivityBook extends AppCompatActivity {
         setButtons();
         setPageViewEdit();
         setFragmentListener();
+
     }
 
     /**
@@ -85,6 +93,20 @@ public class ActivityBook extends AppCompatActivity {
                 setPageCount();
             }
         });
+
+        Button coverButton = findViewById(R.id.add_cover);
+        coverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+    }
+
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
 
     /**
@@ -252,5 +274,14 @@ public class ActivityBook extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.book_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            ((ImageView)findViewById(R.id.cover_book_info)).setImageURI(imageUri);
+        }
     }
 }
